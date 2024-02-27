fa1af8c58db142bc7691f6013d3fee6340f78c4a
mahes <mahes@192.168.1.98>
2024-02-27 12:41:49 +0530

    Dev01 :task01 add mul method

--- GitPro01/src/main/java/in/nit/GitPro01/AppTest.java
+++ GitPro01/src/main/java/in/nit/GitPro01/AppTest.java
@@ -11,8 +11,9 @@ public class AppTest
     public static void main( String[] args )
     {
     	AirthmeticOperation ar=new AirthmeticOperation();
-    	System.out.println(ar.Add(100, 200));
-    	System.out.println(ar.Sub(100,200));
+    	System.out.println("Addition is :"+ar.Add(100, 200));
+    	System.out.println("substration is :"+ar.Sub(100,200));
+    	System.out.println("multiplication is :"+ar.Mul(100,50));
         
     }
 }
--- GitPro01/src/main/java/in/nit/GitPro01/Service/AirthmeticOperation.java
+++ GitPro01/src/main/java/in/nit/GitPro01/Service/AirthmeticOperation.java
@@ -8,5 +8,9 @@ public class AirthmeticOperation {
 	public static int Sub(int x,int  y) {
 		return x-y;
 	}
+	public static int Mul(int x,int y) {
+		return x*y;
+	}
+	
 
 }
